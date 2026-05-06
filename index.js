a3faf5ed004e1ca0264d996ef02661ad2ce4fc6a
fabrizio <fabrizioliberatore7@gmail.com>
2026-05-06 20:56:29 -0300

    menu

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-console.log("Sistema iniciado");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function mostrarMenu() {
+  console.log("\n=== SISTEMA DE VENTAS ===");
+  console.log("1- Registrar venta");
+  console.log("2- Ver stock");
+  console.log("3- Salir");
+
+  rl.question("Seleccione una opción: ", (opcion) => {
+    
+    switch(opcion) {
+      case "1":
+        registrarVenta();
+        break;
+
+      case "2":
+        verStock();
+        break;
+
+      case "3":
+        console.log("Saliendo...");
+        rl.close();
+        break;
+
+      default:
+        console.log("Opción inválida");
+        mostrarMenu();
+    }
+  });
+}
+
+function registrarVenta() {
+  console.log("Venta registrada");
+  mostrarMenu();
+}
+
+function verStock() {
+  console.log("Mostrando stock");
+  mostrarMenu();
+}
+
+mostrarMenu();
