cac570674158d050ed41da6dd8498cf52b99acf9
Sabrina Ayelen Neyra <sabrineyra2008@gmail.com>
2026-05-06 21:00:21 -0300

    Agregué módulo stock

--- stock.js
+++ stock.js
@@ -1,13 +1,34 @@
-let stock = 10;
+let productos = [
+    { nombre: "Teclado", stock: 10 },
+    { nombre: "Mouse", stock: 5 },
+    { nombre: "Monitor", stock: 3 }
+];
 
 function mostrarStock() {
-    console.log("Stock disponible:", stock);
+    console.log("=== STOCK DISPONIBLE ===");
+
+    for (let i = 0; i < productos.length; i++) {
+        console.log(
+            productos[i].nombre + " - Cantidad: " + productos[i].stock
+        );
+    }
 }
 
-function agregarStock(cantidad) {
-    stock = stock + cantidad;
-    console.log("Nuevo stock:", stock);
+function agregarStock(nombreProducto, cantidad) {
+
+    for (let i = 0; i < productos.length; i++) {
+
+        if (productos[i].nombre === nombreProducto) {
+
+            productos[i].stock += cantidad;
+
+            console.log("Stock actualizado");
+        }
+    }
 }
 
 mostrarStock();
-agregarStock(5);
+
+agregarStock("Mouse", 2);
+
+mostrarStock();
